7050eb1dddb2954928e050d182796f0a9758584b
De Freitas <drakoz1987@gmail.com>
2019-04-15 13:11:02 -0400

    Finished time app, regarding Class Components, state, and Life Cycle!

--- time-app/src/App.js
+++ time-app/src/App.js
@@ -1,22 +1,30 @@
 import React,{Component} from 'react';
+import SeasonDisplay from './Components/SeasonDisplay';
+import Loading from './Components/Loading';
+import './App.css'
 
 class App extends Component {
-  state = {lat:null,errorMessage:'',};
+  state = {lat:null,errorMessage:'',month:(new Date().getMonth())+1};
   componentDidMount(){
     window.navigator.geolocation.getCurrentPosition(
       (position) => { this.setState({ lat:position.coords.latitude }) },
       (err) => { this.setState({errorMessage:err.message}) }
     )
   }
-  render(){
+  renderBody = () => {
     if(this.state.errorMessage && !this.state.lat){
       return <div>Error: {this.state.errorMessage}</div>
     }
     if(!this.state.errorMessage && this.state.lat){
-      return <div>Latitude: {this.state.lat}</div>
+      return <SeasonDisplay latitude={this.state.lat} month={this.state.month}/>
     }
-      return <div>Loading!...</div>
+      return <Loading input='Waiting For Your Permission...'/>
+  }
+  render(){
+    return(
+    <div className="App-container">
+      {this.renderBody()}
+    </div>)
   }
 }
-
 export default App;
